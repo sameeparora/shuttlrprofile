fdb9571f812b5cccbc7aaa93cfdc5970a9064afc
sameeparora <arora.sameep@gmail.com>
2017-03-06 19:43:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup
 setup(
     name='shuttlrprofile',    # This is the name of your PyPI-package.
     version='0.2',# Update the version number for new releases
-    packages=['shuttlrprofile'], 
-    scripts=['shuttlrprofile']               # The name of your scipt, and also the command you'll be using for calling it
-      #same as name
+    packages=['shuttlrprofile']
+    
    
 )
