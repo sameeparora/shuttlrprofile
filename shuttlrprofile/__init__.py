0e562f643069c231773128333e9f81c4565604aa
sameeparora <arora.sameep@gmail.com>
2017-03-06 18:40:28 +0530

    Create __init__.py

---
+++ shuttlrprofile/__init__.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='shuttlrprofile',
+      version='0.1',
+      description='Gives user profiles. Waah!',
+      url='http://github.com/sameeparora/shuttlrprofile',
+      author='Sameep Arora',
+      author_email='arora.sameep@gmail.com',
+      license='MIT',
+      packages=['shuttlrprofile'],
+      zip_safe=False)
