6c54808c045ff5e7b0e4cdaef2c6cd3fa74cd148
sameeparora <arora.sameep@gmail.com>
2017-03-06 18:51:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name='shuttlr_profile',    # This is the name of your PyPI-package.
     version='0.1',                          # Update the version number for new releases
-    scripts=['userprofile']                  # The name of your scipt, and also the command you'll be using for calling it
+    scripts=['__init__.py']                  # The name of your scipt, and also the command you'll be using for calling it
 )
